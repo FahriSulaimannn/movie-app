b9c7a74bf24b28f723747c89a276e28e7d04973d
FahriSulaimannn <fahrisulaiman316@gmail.com>
2025-08-14 21:47:06 +0700

    Add Function Trending Movies Feature

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useState, useEffect } from "react";
 import Search from "./components/search";
 import Spinner from "./components/Spinner";
 import MovieCard from "./components/MovieCard";
+import { useDebounce } from "react-use";
+import { updateSearchCount } from "./appwrite";
 
 const API_BASE_URL = 'https://api.themoviedb.org/3';
 
@@ -21,6 +23,9 @@ const App = () => {
   const [errorMessage, setErrorMessage] = useState('');
   const [movieList, setMovieList] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [debouncedSearchTerm, setDebouncedSearchTerm] = useState('');
+
+  useDebounce(() => setDebouncedSearchTerm(searchTerm), 500, [searchTerm]);
 
   const fetchMovies = async (query = '') => {
     setIsLoading(true);
@@ -45,6 +50,10 @@ const App = () => {
       }
 
       setMovieList(data.results || []);
+
+      if (query && data.results && data.results.length > 0) {
+        await updateSearchCount(query, data.results[0]);
+      }
     } catch (error) {
       console.error(`Error fetching movies: ${error}`);
       setErrorMessage(`Error fetching movies. Please try again later`);
@@ -54,8 +63,8 @@ const App = () => {
   }
 
   useEffect(() => {
-      fetchMovies(searchTerm);
-  }, [searchTerm]);
+      fetchMovies(debouncedSearchTerm);
+  }, [debouncedSearchTerm]);
 
   return (
     <main>
